66405e0a180b5ffaae18a984a40f5964022bdaae
John <jjjohncedeno@gmail.com>
2016-12-02 11:02:43 -0500

    Creación del proyecto con la función suma. En este commit se crea el proyecto a utilizar y se reliza la función suma.

---
+++ src/Operaciones.java
@@ -0,0 +1,14 @@
+
+public class Operaciones {
+	private int num1;
+	private int num2;
+	public Operaciones(int num1, int num2) {
+		super();
+		this.num1 = num1;
+		this.num2 = num2;
+	}
+	
+	public int suma(){
+		return this.num1 + this.num2;
+	}
+}
---
+++ src/Programa.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Programa {
+
+	private static Scanner sc;
+
+	public static void main(String[] args) {
+		sc = new Scanner(System.in);
+		int n, n2;
+		System.out.print("Introduzca el primer número entero: ");
+		n = sc.nextInt();
+		System.out.print("Introduzca el segundo número entero: ");
+		n2 = sc.nextInt();
+		Operaciones op= new Operaciones(n,n2);
+		System.out.println("La suma de los números es: " + (op.suma()));
+	}
+}
